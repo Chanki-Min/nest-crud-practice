f042fd624236c7862bde84bdd3457ae8f5911d34
Chanki Min <alscksrl2010@gmail.com>
2021-01-01 22:08:16 +0900

    Add ValidationPile when app starts
    app.module 시작 전에 파라메터 타입 변환 및 DTO 화이트리스트를 위한 validationpipe를 추가하였음

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { BoardModule } from './board/board.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,  //decorator가 붙어있지 않은 body의 attribute를 제거하고 컨트롤러에 넘긴다.
+      forbidNonWhitelisted: true, //whitelist로 걸러질 attribute가 있는 요청을 컨트롤러에 넘기지 않는다.
+      transform: true,  //request의 parameter type을 typescript에 명시한 자료형으로 변환한다.
+    })
+  )
   await app.listen(3000);
 }
 bootstrap();
